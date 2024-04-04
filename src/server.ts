caeaf0fa8f71db3dac0149b9cdb92c01db14a1a6
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-04-04 18:23:30 +0100

    created first post route to create links on database

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import fastify from 'fastify';
+import { z } from 'zod';
+import { sql } from '../lib/postgres';
+
+const app = fastify();
+
+app.post('/links', async (request, reply) => {
+  const createLinkSchema = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  });
+
+  const { code, url } = createLinkSchema.parse(request.body);
+
+  const result =
+    await sql/*sql */ `INSERT INTO url_shortener (code, original_url) VALUES (${code}, ${url}) RETURNING id`;
+
+  const link = result[0];
+
+  return reply.status(201).send({ shortLinkId: link.id });
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log(`HTTP server running on port 3333...`);
+  });
